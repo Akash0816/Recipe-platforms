d49e4c241a8f5b390b56fcd11663520965f21d45
Akash0816 <akashoffical321@gmail.com>
2025-03-24 01:28:52 +0530

    Fixed issues and updated files

--- src/router.js
+++ src/router.js
@@ -1,5 +1,6 @@
 import App from "./App";
-import { createBrowserRouter } from "react-router-dom";
+// import { createBrowserRouter } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./Login";
 import Createrecipe from "./Createrecipe";
 import Search from "./Search";
@@ -9,7 +10,7 @@ import Register from "./Register";
 import Viewrecipe from "./Viewrecipe";
 import EditRecipe from "./EditRecipe";
 
-const router = createBrowserRouter([
+const router = HashRouter([
   { path: "/", element: <App /> },
   { path: "/login", element: <Login /> },
   { path: "/createrecipe", element: <Createrecipe /> },
