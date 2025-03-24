4794a4388e46a779d0be7943c453e29e57e1956e
Akash0816 <akashoffical321@gmail.com>
2025-03-24 13:36:53 +0530

    Fixed router issue

--- src/index.js
+++ src/index.js
@@ -3,25 +3,19 @@ import ReactDOM from "react-dom/client";
 import "./Style/login.css";
 import "font-awesome/css/font-awesome.min.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import RouterComponent from "./router";
 
 import reportWebVitals from "./reportWebVitals";
-import { RouterProvider } from "react-router-dom";
-import router from "./router";
 import { Provider } from "react-redux";
 import { store } from "./store";
+import RouterComponent from "./router"; 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      {/* <RouterProvider router={router} /> */}
-      <RouterComponent />
+      <RouterComponent /> 
     </Provider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
--- src/router.js
+++ src/router.js
@@ -1,31 +1,4 @@
-// import App from "./App";
-// // import { createBrowserRouter } from "react-router-dom";
-// import { HashRouter as Router, Routes, Route } from "react-router-dom";
-// import Login from "./Login";
-// import Createrecipe from "./Createrecipe";
-// import Search from "./Search";
-// import Home from "./Home";
-// import Card from "./Card";
-// import Register from "./Register";
-// import Viewrecipe from "./Viewrecipe";
-// import EditRecipe from "./EditRecipe";
-
-// const router = HashRouter([
-//   { path: "/", element: <App /> },
-//   { path: "/login", element: <Login /> },
-//   { path: "/createrecipe", element: <Createrecipe /> },
-//   { path: "/Search", element: <Search /> },
-//   { path: "/Home", element: <Home /> },
-//   { path: "/card", element: <Card /> },
-//   { path: "/Register", element: <Register /> },
-//   { path: "/recipe/:id", element: <Viewrecipe /> },
-//   { path: "/my-recipe/:id", element: <Viewrecipe /> },
-//   { path: "edit/recipe/:id", element: <EditRecipe /> },
-// ]);
-// export default router;
-
-
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import App from "./App";
 import Login from "./Login";
 import Createrecipe from "./Createrecipe";
@@ -40,11 +13,11 @@ function RouterComponent() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route path="/Recipe-platforms" element={<App />} />{" "}
         <Route path="/login" element={<Login />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/createrecipe" element={<Createrecipe />} />
         <Route path="/search" element={<Search />} />
-        <Route path="/home" element={<Home />} />
         <Route path="/card" element={<Card />} />
         <Route path="/register" element={<Register />} />
         <Route path="/recipe/:id" element={<Viewrecipe />} />
